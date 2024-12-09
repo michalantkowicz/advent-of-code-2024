530ce23d5d2872641bce5bd62b43026d25a9c4ad
michalantkowicz <michal@antkowicz.pl>
2024-12-09 11:08:02 +0100

    day 9 - part 2

--- src/main/java/com/adventofcode/day9/DiskFragmenter.java
+++ src/main/java/com/adventofcode/day9/DiskFragmenter.java
@@ -1,5 +1,8 @@
 package com.adventofcode.day9;
 
+import com.adventofcode.common.Pair;
+
+import java.util.ArrayList;
 import java.util.List;
 
 class DiskFragmenter {
@@ -35,6 +38,69 @@ class DiskFragmenter {
             }
         }
         return checksum;
+    }
+
+    long compactFullFilesAndGetChecksum(List<Pair<Integer>> blocksWithIds) {
+        long checksum = 0;
+        if ((blocksWithIds.size() - 1) % 2 != 0) {
+            throw new IllegalArgumentException("Expected even size of the collection");
+        }
+        int currentFileIndex = blocksWithIds.size() - 1;
+        int currentFileId = blocksWithIds.get(currentFileIndex).b();
+        while (currentFileId > 0) {
+            for (int i = 0; i < currentFileIndex; i++) {
+                if (blocksWithIds.get(i).b().equals(-1)) {
+                    Integer emptyBlockSize = blocksWithIds.get(i).a();
+                    Integer fileBlockSize = blocksWithIds.get(currentFileIndex).a();
+                    if (emptyBlockSize >= fileBlockSize) {
+                        blocksWithIds.add(i, blocksWithIds.get(currentFileIndex));
+                        blocksWithIds.set(i + 1, new Pair<>(emptyBlockSize - fileBlockSize, -1));
+                        blocksWithIds.set(currentFileIndex + 1, new Pair<>(fileBlockSize, -1));
+                        break;
+                    }
+                }
+            }
+
+            blocksWithIds = formatBlocksWithIds(blocksWithIds);
+
+            currentFileId--;
+            for (int i = blocksWithIds.size() - 1; i >= 0; i--) {
+                if (blocksWithIds.get(i).b().equals(currentFileId)) {
+                    currentFileIndex = i;
+                    break;
+                }
+            }
+        }
+        int position = 0;
+        for(Pair<Integer> block : blocksWithIds) {
+            for(int i = 0; i < block.a(); i++) {
+                if(block.b() != -1) {
+                    checksum += (long) position * block.b();
+                }
+                position++;
+            }
+        }
+        return checksum;
+    }
+
+    private static List<Pair<Integer>> formatBlocksWithIds(List<Pair<Integer>> blocksWithIds) {
+        List<Pair<Integer>> formattedBlocksWithIds = new ArrayList<>();
+        int emptyBlockSizeSum = 0;
+        for (Pair<Integer> blocksWithId : blocksWithIds) {
+            if (blocksWithId.b() >= 0) {
+                if (emptyBlockSizeSum > 0) {
+                    formattedBlocksWithIds.add(new Pair<>(emptyBlockSizeSum, -1));
+                    emptyBlockSizeSum = 0;
+                }
+                formattedBlocksWithIds.add(blocksWithId);
+            } else {
+                emptyBlockSizeSum += blocksWithId.a();
+            }
+        }
+        if (emptyBlockSizeSum > 0) {
+            formattedBlocksWithIds.add(new Pair<>(emptyBlockSizeSum, -1));
+        }
+        return formattedBlocksWithIds;
     }
 
     private static int getId(int i) {
--- src/test/java/com/adventofcode/day9/DiskFragmenterTest.java
+++ src/test/java/com/adventofcode/day9/DiskFragmenterTest.java
@@ -1,5 +1,7 @@
 package com.adventofcode.day9;
 
+import com.adventofcode.common.Pair;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -27,11 +29,42 @@ class DiskFragmenterTest {
         assertThat(checksum).isEqualTo(expected);
     }
 
+    @Disabled("Running longer than 1s, run manually if necessary")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideSecondInput")
+    void checksumIsValidWhenMovingWholeFiles(String description, String blocksString, long expected) {
+        // given
+        DiskFragmenter diskFragmenter = new DiskFragmenter();
+        List<Integer> blocks = Arrays.stream(blocksString.split("")).map(Integer::parseInt).toList();
+        List<Pair<Integer>> blocksWithIds = new ArrayList<>();
+        int id = 0;
+        for(int i = 0; i < blocks.size(); i++) {
+            if(i % 2 == 0) {
+                blocksWithIds.add(new Pair<>(blocks.get(i), id++));
+            } else {
+                blocksWithIds.add(new Pair<>(blocks.get(i), -1));
+            }
+        }
+
+        // when
+        long checksum = diskFragmenter.compactFullFilesAndGetChecksum(blocksWithIds);
+
+        // then
+        assertThat(checksum).isEqualTo(expected);
+    }
+
     private static Stream<Arguments> provideFirstInput() {
         return Stream.of(
                 Arguments.of("0_1.in", readFile("/day9/0_1.in"), 60),
                 Arguments.of("0.in", readFile("/day9/0.in"), 1928),
                 Arguments.of("1.in", readFile("/day9/1.in"), 6301895872542L)
+        );
+    }
+
+    private static Stream<Arguments> provideSecondInput() {
+        return Stream.of(
+                Arguments.of("0.in", readFile("/day9/0.in"), 2858),
+                Arguments.of("1.in", readFile("/day9/1.in"), 6323761685944L)
         );
     }
 }
