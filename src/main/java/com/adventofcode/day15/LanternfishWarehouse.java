c47246bf8e64bb6c1f9ed6afd1858957d4528379
mantkowicz <kontakt@antkowicz.pl>
2024-12-16 00:55:40 +0100

    day 15 - part 2

--- src/main/java/com/adventofcode/day15/LanternfishWarehouse.java
+++ src/main/java/com/adventofcode/day15/LanternfishWarehouse.java
@@ -4,21 +4,26 @@ import com.adventofcode.common.Direction;
 import com.adventofcode.common.Pair;
 import com.adventofcode.common.StringMatrix;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LanternfishWarehouse {
     public long getBoxesCoordinatesSum(StringMatrix map, List<Direction> directions) {
         for (Direction direction : directions) {
             Pair<Integer> robotPosition = map.streamIndices().filter(p -> "@".equals(map.at(p))).findFirst().orElseThrow();
-            for (Pair<Integer> toMove : getPositionsToMove(map, robotPosition, direction).reversed()) {
+            List<Pair<Integer>> reversed = getPositionsToMove(map, robotPosition, direction).reversed();
+            for (Pair<Integer> toMove : reversed) {
                 Pair<Integer> moved = direction.moveFrom(toMove);
                 map.set(moved.a(), moved.b(), map.at(toMove));
                 map.set(toMove.a(), toMove.b(), ".");
             }
         }
-        return map.streamIndices().filter(p -> "O".equals(map.at(p))).mapToLong(p -> p.a() + 100L * p.b()).sum();
+        return map.streamIndices().filter(p -> "O".equals(map.at(p))).mapToLong(p -> p.a() + 100L * p.b()).sum() +
+                map.streamIndices().filter(p -> "[".equals(map.at(p))).mapToLong(p -> p.a() + 100L * p.b()).sum();
     }
 
     private List<Pair<Integer>> getPositionsToMove(StringMatrix map, Pair<Integer> position, Direction direction) {
@@ -30,8 +35,35 @@ public class LanternfishWarehouse {
                 List<Pair<Integer>> nextMoves = getPositionsToMove(map, direction.moveFrom(position), direction);
                 yield (nextMoves.isEmpty()) ? Collections.emptyList() : Stream.of(List.of(position), nextMoves).flatMap(List::stream).toList();
             }
+            case "[", "]" -> {
+                if (direction == Direction.LEFT || direction == Direction.RIGHT) {
+                    List<Pair<Integer>> nextMoves = getPositionsToMove(map, direction.moveFrom(position), direction);
+                    yield (nextMoves.isEmpty()) ? Collections.emptyList() : Stream.of(List.of(position), nextMoves).flatMap(List::stream).toList();
+                } else {
+                    List<Pair<Integer>> nextMoves = getPositionsToMove(map, direction.moveFrom(position), direction);
+                    List<Pair<Integer>> nextSideMoves = new ArrayList<>();
+                    if (neighbour.equals("[")) {
+                        nextSideMoves.addAll(getPositionsToMove(map, direction.moveFrom(sum(new Pair<>(1, 0), position)), direction));
+                    } else {
+                        nextSideMoves.addAll(getPositionsToMove(map, direction.moveFrom(sum(new Pair<>(-1, 0), position)), direction));
+                    }
+                    if (nextMoves.isEmpty() || nextSideMoves.isEmpty()) {
+                        yield Collections.emptyList();
+                    } else {
+                        List<Pair<Integer>> result = Stream.of(List.of(position), nextMoves, nextSideMoves).flatMap(List::stream).collect(Collectors.toSet()).stream().toList();
+                        if(direction == Direction.TOP) {
+                            yield result.stream().sorted(Comparator.comparing(Pair<Integer>::b, Comparator.reverseOrder()).thenComparing(Pair::a, Comparator.reverseOrder())).toList();
+                        } else {
+                            yield result.stream().sorted(Comparator.comparing(Pair<Integer>::b).thenComparing(Pair::a, Comparator.reverseOrder())).toList();
+                        }
+                    }
+                }
+            }
             default -> throw new IllegalArgumentException("Unexpected argument: [" + neighbour + "]");
         };
     }
 
+    private static Pair<Integer> sum(Pair<Integer> a, Pair<Integer> b) {
+        return new Pair<>(a.a() + b.a(), a.b() + b.b());
+    }
 }
--- src/test/java/com/adventofcode/day15/LanternfishWarehouseTest.java
+++ src/test/java/com/adventofcode/day15/LanternfishWarehouseTest.java
@@ -30,24 +30,41 @@ class LanternfishWarehouseTest {
         Assertions.assertThat(sum).isEqualTo(expected);
     }
 
-//    @ParameterizedTest(name = "{0}")
-//    @MethodSource("provideSecondInput")
-//    void shouldReturnProperSumOfCoordinatesForWideMap(String description, String input, long expected) {
-//        // given
-//        LanternfishWarehouse warehouse = new LanternfishWarehouse();
-//        String[] inputParts = input.split("(?m)^\\s*$");
-//        StringMatrix map = parseMap(inputParts[0]);
-//        List<Direction> directions = parseDirections(inputParts[1]);
-//
-//        // when
-//        long sum = warehouse.getBoxesCoordinatesSum(map, directions);
-//
-//        // then
-//        Assertions.assertThat(sum).isEqualTo(expected);
-//    }
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideSecondInput")
+    void shouldReturnProperSumOfCoordinatesForWideMap(String description, String input, long expected) {
+        // given
+        LanternfishWarehouse warehouse = new LanternfishWarehouse();
+        String[] inputParts = input.split("(?m)^\\s*$");
+        StringMatrix map = extendMap(inputParts[0]);
+        List<Direction> directions = parseDirections(inputParts[1]);
+
+        // when
+        long sum = warehouse.getBoxesCoordinatesSum(map, directions);
+
+        // then
+        Assertions.assertThat(sum).isEqualTo(expected);
+    }
 
     private StringMatrix parseMap(String input) {
         return new StringMatrix(input);
+    }
+
+    private StringMatrix extendMap(String input) {
+        StringBuilder extended = new StringBuilder();
+        for (String line : input.lines().toList()) {
+            for (String c : line.split("")) {
+                extended.append(switch (c) {
+                    case "#" -> "##";
+                    case "O" -> "[]";
+                    case "." -> "..";
+                    case "@" -> "@.";
+                    default -> throw new IllegalArgumentException("Unexpected argument: [" + c + "]");
+                });
+            }
+            extended.append("\n");
+        }
+        return new StringMatrix(extended.toString());
     }
 
     private List<Direction> parseDirections(String input) {
@@ -78,9 +95,9 @@ class LanternfishWarehouseTest {
 
     private static Stream<Arguments> provideSecondInput() {
         return Stream.of(
-                Arguments.of("0_1.in", readFile("/day15/0_1.in"), 105),
+                Arguments.of("0_2.in", readFile("/day15/0_2.in"), 618),
                 Arguments.of("0.in", readFile("/day15/0.in"), 9021),
-                Arguments.of("1.in", readFile("/day15/1.in"), -1)
+                Arguments.of("1.in", readFile("/day15/1.in"), 1392847)
         );
     }
 }
