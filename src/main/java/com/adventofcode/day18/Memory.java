779af1ba74deb1a044401bdef2969154732b6bb6
mantkowicz <kontakt@antkowicz.pl>
2024-12-18 22:30:22 +0100

    day 18 - part 2

--- src/main/java/com/adventofcode/day18/Memory.java
+++ src/main/java/com/adventofcode/day18/Memory.java
@@ -8,9 +8,8 @@ import java.util.*;
 
 class Memory {
     private static final Map<Pair<Integer>, Long> distance = new HashMap<>();
-    private static final Map<Pair<Integer>, Pair<Integer>> previous = new HashMap<>();
 
-    public long calculateShortestPath(IntMatrix space, List<Pair<Integer>> bytes, int limit) {
+    long calculateShortestPath(IntMatrix space, List<Pair<Integer>> bytes, int limit) {
         Pair<Integer> start = new Pair<>(0, 0);
         Pair<Integer> end = new Pair<>(space.width() - 1, space.height() - 1);
 
@@ -19,6 +18,23 @@ class Memory {
         minCost(space, start);
 
         return distance.get(end);
+    }
+
+    Pair<Integer> calculateFirstByte(IntMatrix space, List<Pair<Integer>> bytes, int limit) {
+        Pair<Integer> start = new Pair<>(0, 0);
+        Pair<Integer> end = new Pair<>(space.width() - 1, space.height() - 1);
+
+        bytes.stream().limit(limit).forEach(b -> space.set(b.a(), b.b(), -1));
+        Pair<Integer> currentByte;
+
+        do {
+            currentByte = bytes.get(limit++);
+            space.set(currentByte.a(), currentByte.b(), -1);
+            distance.clear();
+            minCost(space, start);
+        } while (distance.get(end) < Long.MAX_VALUE);
+
+        return currentByte;
     }
 
 
@@ -33,6 +49,9 @@ class Memory {
         while (!Q.isEmpty()) {
             Pair<Integer> p = getMin(Q);
             Q.remove(p);
+            if (distance.get(p) == Long.MAX_VALUE) {
+                continue;
+            }
 
             Arrays.stream(Direction.values())
                     .map(d -> d.moveFrom(p))
@@ -42,7 +61,6 @@ class Memory {
                     .forEach(n -> {
                         if (distance.get(n) > distance.get(p) + 1) {
                             distance.put(n, distance.get(p) + 1);
-                            previous.put(n, p);
                         }
                     });
         }
--- src/test/java/com/adventofcode/day18/MemoryTest.java
+++ src/test/java/com/adventofcode/day18/MemoryTest.java
@@ -26,10 +26,31 @@ class MemoryTest {
         Assertions.assertThat(shortestPath).isEqualTo(expected);
     }
 
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideSecondInput")
+    void shouldReturnProperByte(String description, int limit, IntMatrix space, List<Pair<Integer>> bytes, Pair<Integer> expected) {
+        // given
+        Memory memory = new Memory();
+
+        // when
+        Pair<Integer> b = memory.calculateFirstByte(space, bytes, limit);
+
+        // then
+        Assertions.assertThat(b).isNotEqualTo(bytes.get(limit));
+        Assertions.assertThat(b).isEqualTo(expected);
+    }
+
     private static Stream<Arguments> provideFirstInput() {
         return Stream.of(
                 Arguments.of("0.in", 12, new IntMatrix(7, 7, 0), getIntPairs("/day18/0.in", ","), 22),
                 Arguments.of("1.in", 1024, new IntMatrix(71, 71, 0), getIntPairs("/day18/1.in", ","), 340)
+        );
+    }
+
+    private static Stream<Arguments> provideSecondInput() {
+        return Stream.of(
+                Arguments.of("0.in", 0, new IntMatrix(7, 7, 0), getIntPairs("/day18/0.in", ","), new Pair<>(6, 1)),
+                Arguments.of("1.in", 2910, new IntMatrix(71, 71, 0), getIntPairs("/day18/1.in", ","), new Pair<>(34, 32))
         );
     }
 }
