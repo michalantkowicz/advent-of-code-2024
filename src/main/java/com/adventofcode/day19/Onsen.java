a3f1847f0c880ba47d253e39aa8a3461e4d1fb63
mantkowicz <kontakt@antkowicz.pl>
2024-12-23 17:53:30 +0100

    day 19 - part 2

--- src/main/java/com/adventofcode/day19/Onsen.java
+++ src/main/java/com/adventofcode/day19/Onsen.java
@@ -1,6 +1,7 @@
 package com.adventofcode.day19;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,5 +22,41 @@ class Onsen {
             }
         }
         return false;
+    }
+
+    Map<String, Long> CACHE = new HashMap<>();
+
+    long countAllPossibleArrangements(List<String> towels, List<String> designs) {
+        long sum = 0;
+        for (String design : designs) {
+            List<String> filtered = towels.stream().filter(design::contains).toList();
+            for (String f : filtered) {
+                if (design.startsWith(f)) {
+                    long d = d(design, f, filtered);
+
+                    sum += d;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private long d(String design, String prefix, List<String> towels) {
+        long result = 0;
+        if (prefix.equals(design)) {
+            result++;
+        } else if (prefix.length() < design.length()) {
+            for (String towel : towels) {
+                String newPrefix = prefix + towel;
+                if (design.startsWith(newPrefix) && design.length() >= newPrefix.length()) {
+                    String key = design.substring(newPrefix.length());
+                    if (!CACHE.containsKey(key)) {
+                        CACHE.put(key, d(design, newPrefix, towels));
+                    }
+                    result += CACHE.get(key);
+                }
+            }
+        }
+        return result;
     }
 }
--- src/test/java/com/adventofcode/day19/OnsenTest.java
+++ src/test/java/com/adventofcode/day19/OnsenTest.java
@@ -25,6 +25,19 @@ class OnsenTest {
         Assertions.assertThat(count).isEqualTo(expected);
     }
 
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideSecondInput")
+    void shouldReturnProperCountOfAllPossibleArrangements(String description, List<String> towels, List<String> designs, long expected) {
+        // given
+        Onsen onsen = new Onsen();
+
+        // when
+        long count = onsen.countAllPossibleArrangements(towels, designs);
+
+        // then
+        Assertions.assertThat(count).isEqualTo(expected);
+    }
+
     private static List<String> parseTowels(String input) {
         String firstLine = input.lines().limit(1).findFirst().orElseThrow();
         return Arrays.stream(firstLine.split(",")).map(String::trim).toList();
@@ -44,7 +57,7 @@ class OnsenTest {
     private static Stream<Arguments> provideSecondInput() {
         return Stream.of(
                 Arguments.of("0.in", parseTowels(readFile("/day19/0.in")), parseDesigns(readFile("/day19/0.in")), 16),
-                Arguments.of("1.in", parseTowels(readFile("/day19/1.in")), parseDesigns(readFile("/day19/1.in")), -1)
+                Arguments.of("1.in", parseTowels(readFile("/day19/1.in")), parseDesigns(readFile("/day19/1.in")), 635018909726691L)
         );
     }
 }
